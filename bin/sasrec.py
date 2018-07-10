094dcd6ed0e7e9f595dc6d526895c43afbfd18c7
Thomas D. Grant <tgrant@hwi.buffalo.edu>
2018-07-10 17:25:29 -0400

    Added draft version of sasrec.py script (likely to be renamed in the future) to fit scattering profiles with an extended version of trigonometric series based on Moore 1979 algorithm and calculate pair distribution function, as well as other useful parameters. Includes alpha smoothing factor. Includes a simple interactive GUI using matplotlib. Manuscript describing math and method in preparation.
    Added rho2dat.py script to calculate simple scattering profiles from
    electron density maps. Currently only works with cubic maps (i.e. same
    length and shape on all sides). Includes new read_mrc() function in
    saxstats module.
    
    Updated setup.py to reflect new scripts.
    
    Minor version bump to 1.3.0.

---
+++ bin/sasrec.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+
+import datetime, time
+import os, argparse, sys, imp
+import logging
+import numpy as np
+from saxstats._version import __version__
+import saxstats.saxstats as saxs
+print saxs.__file__
+try:
+    imp.find_module('matplotlib')
+    matplotlib_found = True
+except ImportError:
+    matplotlib_found = False
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--version", action="version",version="%(prog)s v{version}".format(version=__version__))
+parser.add_argument("-f", "--file", type=str, help="SAXS data file for input (either .dat or .out)")
+parser.add_argument("-d", "--dmax", default=100., type=float, help="Estimated maximum dimension")
+parser.add_argument("-a", "--alpha", default=0., type=float, help="Set alpha smoothing factor")
+parser.add_argument("--nes", default=2, type=int, help=argparse.SUPPRESS)
+parser.add_argument("--max_dmax", default=200., type=float, help="Maximum limit for allowed Dmax values (for plotting slider)")
+parser.add_argument("--max_alpha", default=10., type=float, help="Maximum limit for allowed alpha values (for plotting slider)")
+parser.add_argument("--max_nes", default=10, type=int, help=argparse.SUPPRESS)
+parser.add_argument("--no_gui", dest="plot", action="store_false", help="Do not run the interactive GUI mode.")
+parser.add_argument("-o", "--output", default=None, help="Output filename prefix")
+if matplotlib_found:
+    parser.set_defaults(plot=True)
+else:
+    parser.set_defaults(plot=False)
+args = parser.parse_args()
+
+alpha = args.alpha
+
+if args.output is None:
+    basename, ext = os.path.splitext(args.file)
+    output = basename + '_fit'
+else:
+    output = args.output
+
+Iq = np.loadtxt(args.file)
+D = args.dmax
+nes = args.nes
+
+q = Iq[:,0]
+qmin = np.min(q[0])
+dq = q[1] - q[0]
+nq = int(qmin/dq)
+qc = np.concatenate(([0.0],np.arange(nq)*dq+(qmin-nq*dq),q))
+Icerr = np.concatenate((np.ones(nq+1)*Iq[0,2],Iq[:,2]))
+sasrec = saxs.Sasrec(Iq, D, qc=qc, r=None, alpha=alpha, ne=nes)
+
+
+if args.plot:
+    import matplotlib.pyplot as plt
+    from matplotlib.widgets import Slider, Button, RadioButtons
+    
+    fig, (axI, axP) = plt.subplots(1, 2, figsize=(12,6))
+    plt.subplots_adjust(left=0.065, bottom=0.25, right=0.98, top=0.95)
+
+    I_l1, = axI.plot(sasrec.q, sasrec.I, 'k.')
+    I_l2, = axI.plot(sasrec.qc, sasrec.Ic, 'r-', lw=2)
+    axI.semilogy()
+    axI.set_ylabel('I(q)')
+    axI.set_xlabel('q')
+
+    P_l1, = axP.plot(sasrec.r*100, sasrec.r*0, 'k--')
+    P_l2, = axP.plot(sasrec.r, sasrec.P, 'b-', lw=2)
+    axP.set_ylabel('P(r)')
+    axP.set_xlabel('r')
+
+    axI.set_xlim([0,1.1*np.max(sasrec.q)])
+    axP.set_xlim([0,1.1*np.max(sasrec.r)])
+
+    axcolor = 'lightgoldenrodyellow'
+    axdmax = plt.axes([0.05, 0.125, 0.4, 0.03], facecolor=axcolor)
+    axalpha = plt.axes([0.05, 0.075, 0.4, 0.03], facecolor=axcolor)
+    #axnes = plt.axes([0.05, 0.025, 0.4, 0.03], facecolor=axcolor)
+
+    axrg = plt.figtext(.55, .125, "Rg = " + str(round(sasrec.rg,2)) + " +- " + str(round(sasrec.rgerr,2)))
+    axI0 = plt.figtext(.75, .125, "I(0) = " + str(round(sasrec.I0,2)) + " +- " + str(round(sasrec.I0err,2)))
+    axVp = plt.figtext(.55, .075, "Vp = " + str(round(sasrec.Vp,2)) + " +- " + str(round(sasrec.Vperr,2)))
+    axVpmw = plt.figtext(.75, .075, "Vp MW = " + str(round(sasrec.mwVp,2)) + " +- " + str(round(sasrec.mwVperr,2)))
+    axVcmw = plt.figtext(.55, .025, "Vc MW = " + str(round(sasrec.mwVc,2)) + " +- " + str(round(sasrec.mwVcerr,2)))
+    axlc = plt.figtext(.75, .025, "Lc = " + str(round(sasrec.lc,2)) + " +- " + str(round(sasrec.lcerr,2)))
+
+    sdmax = Slider(axdmax, 'Dmax', 0.0, args.max_dmax, valinit=D, valstep=D/1000.)
+    salpha = Slider(axalpha, 'Alpha', 0.0, args.max_alpha, valinit=alpha, valstep=alpha/100.)
+    #snes = Slider(axnes, 'NES', 0, args.max_nes, valinit=args.nes, valstep=1)
+
+    def update(val):
+        dmax = sdmax.val
+        alpha = salpha.val
+        #nes = int(snes.val)
+        global sasrec
+        sasrec = saxs.Sasrec(Iq, dmax, qc=qc, r=None, alpha=alpha, ne=nes)
+        I_l2.set_data(sasrec.qc, sasrec.Ic)
+        P_l2.set_data(sasrec.r, sasrec.P)
+        axrg.set_text("Rg = " + str(round(sasrec.rg,2)) + " +- " + str(round(sasrec.rgerr,2)))
+        axI0.set_text("I(0) = " + str(round(sasrec.I0,2)) + " +- " + str(round(sasrec.I0err,2)))
+        axVp.set_text("Vp = " + str(round(sasrec.Vp,2)) + " +- " + str(round(sasrec.Vperr,2)))
+        axVpmw.set_text("Vp MW = " + str(round(sasrec.mwVp,2)) + " +- " + str(round(sasrec.mwVperr,2)))
+        axVcmw.set_text("Vc MW = " + str(round(sasrec.mwVc,2)) + " +- " + str(round(sasrec.mwVcerr,2)))
+        axlc.set_text("Lc = " + str(round(sasrec.lc,2)) + " +- " + str(round(sasrec.lcerr,2)))
+        #axI.set_ylim([0.9*np.min(sasrec.Ic),1.1*np.max(sasrec.Ic)])
+        #axP.set_xlim([0,1.1*np.max(sasrec.r)])
+        fig.canvas.draw_idle()
+    sdmax.on_changed(update)
+    salpha.on_changed(update)
+    #snes.on_changed(update)
+
+    axreset = plt.axes([0.05, 0.02, 0.1, 0.04])
+    reset_button = Button(axreset, 'Reset Sliders', color=axcolor, hovercolor='0.975')
+
+    def reset_values(event):
+        sdmax.reset()
+        salpha.reset()
+    reset_button.on_clicked(reset_values)
+
+    axprint = plt.axes([0.2, 0.02, 0.1, 0.04])
+    print_button = Button(axprint, 'Print Values', color=axcolor, hovercolor='0.975')
+
+    def print_values(event):
+        print "---------------------------------"
+        print "Dmax = " + str(round(sasrec.D,2))
+        print "alpha = %.5e" % sasrec.alpha
+        print "Rg = " + str(round(sasrec.rg,2)) + " +- " + str(round(sasrec.rgerr,2))
+        print "I(0) = " + str(round(sasrec.I0,2)) + " +- " + str(round(sasrec.I0err,2))
+        print "Vp = " + str(round(sasrec.Vp,2)) + " +- " + str(round(sasrec.Vperr,2))
+        print "Vp MW = " + str(round(sasrec.mwVp,2)) + " +- " + str(round(sasrec.mwVperr,2))
+        print "Vc MW = " + str(round(sasrec.mwVc,2)) + " +- " + str(round(sasrec.mwVcerr,2))
+        print "Lc = " + str(round(sasrec.lc,2)) + " +- " + str(round(sasrec.lcerr,2))
+    print_button.on_clicked(print_values)
+
+    axsave = plt.axes([0.35, 0.02, 0.1, 0.04])
+    save_button = Button(axsave, 'Save File', color=axcolor, hovercolor='0.975')
+
+    def save_file(event):
+        #sascif = saxs.Sascif(sasrec)
+        #sascif.write(output+".sascif")
+        #print "%s file saved" % (output+".sascif")
+        np.savetxt(output+'.dat', np.vstack((sasrec.qc, sasrec.Ic, Icerr)).T,delimiter=' ',fmt='%.5e')
+        print "%s file saved" % (output+".dat")
+    save_button.on_clicked(save_file)
+
+    plt.show()
+
+
+print "---------------------------------"
+print "Dmax = " + str(round(sasrec.D,2))
+print "alpha = %.5e" % sasrec.alpha
+print "Rg = " + str(round(sasrec.rg,2)) + " +- " + str(round(sasrec.rgerr,2))
+print "I(0) = " + str(round(sasrec.I0,2)) + " +- " + str(round(sasrec.I0err,2))
+print "Vp = " + str(round(sasrec.Vp,2)) + " +- " + str(round(sasrec.Vperr,2))
+print "Vp MW = " + str(round(sasrec.mwVp,2)) + " +- " + str(round(sasrec.mwVperr,2))
+print "Vc MW = " + str(round(sasrec.mwVc,2)) + " +- " + str(round(sasrec.mwVcerr,2))
+print "Lc = " + str(round(sasrec.lc,2)) + " +- " + str(round(sasrec.lcerr,2))
+
+
+#sascif = saxs.Sascif(sasrec)
+#sascif.write(output+".sascif")
+#print "%s file saved" % (output+".sascif")
+
+np.savetxt(output+'.dat', np.vstack((sasrec.qc, sasrec.Ic, Icerr)).T,delimiter=' ',fmt='%.5e')
+print "%s file saved" % (output+".dat")
+
+
+
+
+
+
+
+
+
+
+
+
