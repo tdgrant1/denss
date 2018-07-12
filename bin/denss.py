0e97d8d1662350f32771d809e65a4910a698cfac
Thomas D. Grant <tgrant@hwi.buffalo.edu>
2018-07-12 14:53:20 -0400

    Added new denssopts argument parser module to enable passing arguments between different scripts.

--- bin/denss.py
+++ bin/denss.py
@@ -27,6 +27,7 @@
 
 from saxstats._version import __version__
 import saxstats.saxstats as saxs
+import saxstats.denssopts as dopts
 import numpy as np
 import sys, argparse, os
 import logging
@@ -41,152 +42,27 @@ except ImportError:
     matplotlib_found = False
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--version", action="version",version="%(prog)s v{version}".format(version=__version__))
-parser.add_argument("-f", "--file", type=str, help="SAXS data file for input (either .dat or .out)")
-parser.add_argument("-d", "--dmax", default=None, type=float, help="Estimated maximum dimension")
-parser.add_argument("-v", "--voxel", default=None, type=float, help="Set desired voxel size, setting resolution of map")
-parser.add_argument("-os","--oversampling", default=3., type=float, help="Sampling ratio")
-parser.add_argument("-n", "--nsamples", default=None, type=int, help="Number of samples, i.e. grid points, along a single dimension. (Sets voxel size, overridden by --voxel. Best optimization with n=power of 2)")
-parser.add_argument("--ne", default=10000, type=float, help="Number of electrons in object")
-parser.add_argument("-s", "--steps", default=None, help="Maximum number of steps (iterations)")
-parser.add_argument("-o", "--output", default=None, help="Output map filename")
-parser.add_argument("-m", "--mode", default="SLOW", type=str, help="Mode. F(AST) sets default options to run quickly for simple particle shapes. S(LOW) useful for more complex molecules. (default SLOW)")
-parser.add_argument("--seed", default=None, help="Random seed to initialize the map")
-parser.add_argument("-ld_on","--limit_dmax_on", dest="limit_dmax", action="store_true", help="Limit electron density to sphere of radius 0.6*Dmax from center of object.")
-parser.add_argument("-ld_off","--limit_dmax_off", dest="limit_dmax", action="store_false", help="Do not limit electron density to sphere of radius 0.6*Dmax from center of object. (default)")
-parser.add_argument("-ld_steps","--limit_dmax_steps", default=None, type=int, nargs='+', help="List of steps for limiting density to sphere of Dmax (default=500)")
-parser.add_argument("-rc_on", "--recenter_on", dest="recenter", action="store_true", help="Recenter electron density when updating support. (default)")
-parser.add_argument("-rc_off", "--recenter_off", dest="recenter", action="store_false", help="Do not recenter electron density when updating support.")
-parser.add_argument("-rc_steps", "--recenter_steps", default=None, type=int, nargs='+', help="List of steps to recenter electron density.")
-parser.add_argument("-rc_mode", "--recenter_mode", default="com", type=str, help="Recenter based on either center of mass (com, default) or maximum density value (max)")
-parser.add_argument("-p_on","--positivity_on", dest="positivity", action="store_true", help="Enforce positivity restraint inside support. (default)")
-parser.add_argument("-p_off","--positivity_off", dest="positivity", action="store_false", help="Do not enforce positivity restraint inside support.")
-parser.add_argument("-min","--minimum_density", default=None, type=float, help="Minimum density value in e-/angstrom^3 (must also set --ne to be meaningful)")
-parser.add_argument("-max","--maximum_density", default=None, type=float, help="Maximum density value in e-/angstrom^3 (must also set --ne to be meaningful)")
-parser.add_argument("-e_on","--extrapolate_on", dest="extrapolate", action="store_true", help="Extrapolate data by Porod law to high resolution limit of voxels. (default)")
-parser.add_argument("-e_off","--extrapolate_off", dest="extrapolate", action="store_false", help="Do not extrapolate data by Porod law to high resolution limit of voxels.")
-parser.add_argument("-sw_on","--shrinkwrap_on", dest="shrinkwrap", action="store_true", help="Turn shrinkwrap on (default)")
-parser.add_argument("-sw_off","--shrinkwrap_off", dest="shrinkwrap", action="store_false", help="Turn shrinkwrap off")
-parser.add_argument("-sw_start","--shrinkwrap_sigma_start", default=3, type=float, help="Starting sigma for Gaussian blurring, in voxels")
-parser.add_argument("-sw_end","--shrinkwrap_sigma_end", default=1.5, type=float, help="Ending sigma for Gaussian blurring, in voxels")
-parser.add_argument("-sw_decay","--shrinkwrap_sigma_decay", default=0.99, type=float, help="Rate of decay of sigma, fraction")
-parser.add_argument("-sw_threshold","--shrinkwrap_threshold_fraction", default=0.20, type=float, help="Minimum threshold defining support, in fraction of maximum density")
-parser.add_argument("-sw_iter","--shrinkwrap_iter", default=20, type=int, help="Number of iterations between updating support with shrinkwrap")
-parser.add_argument("-sw_minstep","--shrinkwrap_minstep", default=None, type=int, help="First step to begin shrinkwrap")
-parser.add_argument("-ec_on","--enforce_connectivity_on", dest="enforce_connectivity", action="store_true", help="Enforce connectivity of support, i.e. remove extra blobs (default)")
-parser.add_argument("-ec_off","--enforce_connectivity_off", dest="enforce_connectivity", action="store_false", help="Do not enforce connectivity of support")
-parser.add_argument("-ec_steps","--enforce_connectivity_steps", default=None, type=int, nargs='+', help="List of steps to enforce connectivity")
-parser.add_argument("--chi_end_fraction", default=0.001, type=float, help="Convergence criterion. Minimum threshold of chi2 std dev, as a fraction of the median chi2 of last 100 steps.")
-parser.add_argument("--write_xplor", default=False, action="store_true", help="Write out XPLOR map format (default only write MRC format).")
-parser.add_argument("--write_freq", default=100, type=int, help="How often to write out current density map (in steps, default 100).")
-parser.add_argument("--cutout_on", dest="cutout", action="store_true", help="When writing final map, cut out the particle to make smaller files.")
-parser.add_argument("--cutout_off", dest="cutout", action="store_false", help="When writing final map, do not cut out the particle to make smaller files (default).")
-parser.add_argument("--plot_on", dest="plot", action="store_true", help="Create simple plots of results (requires Matplotlib, default if module exists).")
-parser.add_argument("--plot_off", dest="plot", action="store_false", help="Do not create simple plots of results. (Default if Matplotlib does not exist)")
-parser.set_defaults(limit_dmax=False)
-parser.set_defaults(shrinkwrap=True)
-parser.set_defaults(recenter=True)
-parser.set_defaults(positivity=True)
-parser.set_defaults(extrapolate=True)
-parser.set_defaults(enforce_connectivity=True)
-parser.set_defaults(cutout=False)
-if matplotlib_found:
-    parser.set_defaults(plot=True)
-else:
-    parser.set_defaults(plot=False)
-args = parser.parse_args()
+args = dopts.parse_arguments(parser)
 
-if args.output is None:
-    basename, ext = os.path.splitext(args.file)
-    output = basename
-else:
-    output = args.output
-
-logging.basicConfig(filename=output+'.log',level=logging.INFO,filemode='w',
+logging.basicConfig(filename=args.output+'.log',level=logging.INFO,filemode='w',
     format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p')
 logging.info('BEGIN')
 logging.info('DENSS Version: %s', __version__)
 logging.info('Data filename: %s', args.file)
-logging.info('Output prefix: %s', output)
-
-#A bug appears to be present when disabling the porod extrapolation.
-#for now that option will be disabled until I come up with a fix
-if args.extrapolate is False:
-    print ("There is currently a bug when disabling the Porod "
-           "extrapolation (the -e_off option). \n For now, extrapolation "
-           "has been re-enabled until a bug fix is released. ")
-    args.extrapolate = True
+logging.info('Output prefix: %s', args.output)
 
 q, I, sigq, dmax, isout = saxs.loadProfile(args.file)
 
-#for FAST or SLOW modes, set some default values for a few options
-if args.mode[0].upper() == "F":
-    mode = "FAST"
-    nsamples = 32
-    shrinkwrap_minstep = 1000
-    enforce_connectivity_steps = [2000]
-    recenter_steps = range(501,2502,500)
-    steps = 5000
-elif args.mode[0].upper() == "S":
-    mode = "SLOW"
-    nsamples = 64
-    shrinkwrap_minstep = 5000
-    enforce_connectivity_steps = [6000]
-    recenter_steps = range(501,8002,500)
-    steps = 10000
-else:
-    mode = "None"
-
-#allow user to explicitly modify those values by resetting them here to the user defined values
-if args.nsamples is not None:
-    nsamples = args.nsamples
-
-if args.shrinkwrap_minstep is not None:
-    shrinkwrap_minstep = args.shrinkwrap_minstep
-
-if args.enforce_connectivity_steps is not None:
-    enforce_connectivity_steps = args.enforce_connectivity_steps
-if not isinstance(enforce_connectivity_steps, list):
-    enforce_connectivity_steps = [ enforce_connectivity_steps ]
-
-if args.recenter_steps is not None:
-    recenter_steps = args.recenter_steps
-if not isinstance(recenter_steps, list):
-    recenter_steps = [ recenter_steps ]
-
-if args.limit_dmax_steps is not None:
-    limit_dmax_steps = args.limit_dmax_steps
-else:
-    limit_dmax_steps = [500]
-if not isinstance(limit_dmax_steps, list):
-    limit_dmax_steps = [ limit_dmax_steps ]
-
-if args.steps is not None:
-    steps = args.steps
-
-if args.dmax is not None:
-    dmax = args.dmax
-elif dmax<=0:
-    dmax = 100
-
-if args.voxel is None and nsamples is None:
-    voxel = 5.
-elif args.voxel is None and nsamples is not None:
-    voxel = dmax * args.oversampling / nsamples
-else:
-    voxel = args.voxel
-
-
-logging.info('Mode: %s', mode)
+logging.info('Mode: %s', args.mode)
 logging.info('q range of input data: %3.3f < q < %3.3f', q.min(), q.max())
-logging.info('Maximum dimension: %3.3f', dmax)
+logging.info('Maximum dimension: %3.3f', args.dmax)
 logging.info('Sampling ratio: %3.3f', args.oversampling)
-logging.info('Requested real space voxel size: %3.3f', voxel)
+logging.info('Requested real space voxel size: %3.3f', args.voxel)
 logging.info('Number of electrons: %3.3f', args.ne)
 logging.info('Limit Dmax: %s', args.limit_dmax)
-logging.info('Limit Dmax Steps: %s', limit_dmax_steps)
+logging.info('Limit Dmax Steps: %s', args.limit_dmax_steps)
 logging.info('Recenter: %s', args.recenter)
-logging.info('Recenter Steps: %s', recenter_steps)
+logging.info('Recenter Steps: %s', args.recenter_steps)
 logging.info('Recenter Mode: %s', args.recenter_mode)
 logging.info('Positivity: %s', args.positivity)
 logging.info('Minimum Density: %s', args.minimum_density)
@@ -198,29 +74,29 @@ logging.info('Shrinkwrap sigma end: %s', args.shrinkwrap_sigma_end)
 logging.info('Shrinkwrap sigma decay: %s', args.shrinkwrap_sigma_decay)
 logging.info('Shrinkwrap threshold fraction: %s', args.shrinkwrap_threshold_fraction)
 logging.info('Shrinkwrap iterations: %s', args.shrinkwrap_iter)
-logging.info('Shrinkwrap starting step: %s', shrinkwrap_minstep)
+logging.info('Shrinkwrap starting step: %s', args.shrinkwrap_minstep)
 logging.info('Enforce connectivity: %s', args.enforce_connectivity)
-logging.info('Enforce connectivity steps: %s', enforce_connectivity_steps)
+logging.info('Enforce connectivity steps: %s', args.enforce_connectivity_steps)
 logging.info('Chi2 end fraction: %3.3e', args.chi_end_fraction)
 
 qdata, Idata, sigqdata, qbinsc, Imean, chis, rg, supportV = saxs.denss(
     q=q,I=I,sigq=sigq,
-    D=dmax,
+    D=args.dmax,
     ne=args.ne,
-    voxel=voxel,
+    voxel=args.voxel,
     oversampling=args.oversampling,
     limit_dmax=args.limit_dmax,
-    limit_dmax_steps=limit_dmax_steps,
+    limit_dmax_steps=args.limit_dmax_steps,
     recenter=args.recenter,
-    recenter_steps=recenter_steps,
+    recenter_steps=args.recenter_steps,
     recenter_mode=args.recenter_mode,
     positivity=args.positivity,
     rho_min=args.minimum_density,
     rho_max=args.maximum_density,
     extrapolate=args.extrapolate,
     write=True,
-    filename=output,
-    steps=steps,
+    filename=args.output,
+    steps=args.steps,
     seed=args.seed,
     shrinkwrap=args.shrinkwrap,
     shrinkwrap_sigma_start=args.shrinkwrap_sigma_start,
@@ -228,15 +104,15 @@ qdata, Idata, sigqdata, qbinsc, Imean, chis, rg, supportV = saxs.denss(
     shrinkwrap_sigma_decay=args.shrinkwrap_sigma_decay,
     shrinkwrap_threshold_fraction=args.shrinkwrap_threshold_fraction,
     shrinkwrap_iter=args.shrinkwrap_iter,
-    shrinkwrap_minstep=shrinkwrap_minstep,
+    shrinkwrap_minstep=args.shrinkwrap_minstep,
     chi_end_fraction=args.chi_end_fraction,
     write_xplor_format=args.write_xplor,
     write_freq=args.write_freq,
     enforce_connectivity=args.enforce_connectivity,
-    enforce_connectivity_steps=enforce_connectivity_steps,
+    enforce_connectivity_steps=args.enforce_connectivity_steps,
     cutout=args.cutout)
 
-print output
+print args.output
 
 fit = np.zeros(( len(qbinsc),5 ))
 fit[:len(qdata),0] = qdata
@@ -244,8 +120,8 @@ fit[:len(Idata),1] = Idata
 fit[:len(sigqdata),2] = sigqdata
 fit[:len(qbinsc),3] = qbinsc
 fit[:len(Imean),4] = Imean
-np.savetxt(output+'_map.fit',fit,delimiter=' ',fmt='%.5e', header='q(data),I(data),error(data),q(density),I(density)')
-np.savetxt(output+'_stats_by_step.dat',np.vstack((chis, rg, supportV)).T,delimiter=" ",fmt="%.5e",header='Chi2 Rg SupportVolume')
+np.savetxt(args.output+'_map.fit',fit,delimiter=' ',fmt='%.5e', header='q(data),I(data),error(data),q(density),I(density)')
+np.savetxt(args.output+'_stats_by_step.dat',np.vstack((chis, rg, supportV)).T,delimiter=" ",fmt="%.5e",header='Chi2 Rg SupportVolume')
 
 if args.plot and matplotlib_found:
     f = plt.figure(figsize=[6,6])
@@ -281,7 +157,7 @@ if args.plot and matplotlib_found:
     ax1.set_xlabel(r'q ($\mathrm{\AA^{-1}}$)')
     #plt.setp(ax0.get_xticklabels(), visible=False)
     plt.tight_layout()
-    plt.savefig(output+'_fit',ext='png',dpi=150)
+    plt.savefig(args.output+'_fit',ext='png',dpi=150)
     plt.close()
 
     plt.plot(chis[chis>0])
@@ -289,14 +165,14 @@ if args.plot and matplotlib_found:
     plt.ylabel('$\chi^2$')
     plt.semilogy()
     plt.tight_layout()
-    plt.savefig(output+'_chis',ext='png',dpi=150)
+    plt.savefig(args.output+'_chis',ext='png',dpi=150)
     plt.close()
 
     plt.plot(rg[rg!=0])
     plt.xlabel('Step')
     plt.ylabel('Rg')
     plt.tight_layout()
-    plt.savefig(output+'_rgs',ext='png',dpi=150)
+    plt.savefig(args.output+'_rgs',ext='png',dpi=150)
     plt.close()
 
     plt.plot(supportV[supportV>0])
@@ -304,7 +180,7 @@ if args.plot and matplotlib_found:
     plt.ylabel('Support Volume ($\mathrm{\AA^{3}}$)')
     plt.semilogy()
     plt.tight_layout()
-    plt.savefig(output+'_supportV',ext='png',dpi=150)
+    plt.savefig(args.output+'_supportV',ext='png',dpi=150)
     plt.close()
 
 logging.info('END')
---
+++ saxstats/denssopts.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+
+from _version import __version__
+import os, argparse
+import imp
+try:
+    imp.find_module('matplotlib')
+    matplotlib_found = True
+    import matplotlib.pyplot as plt
+    from  matplotlib.colors import colorConverter as cc
+    import matplotlib.gridspec as gridspec
+except ImportError:
+    matplotlib_found = False
+
+def parse_arguments(parser):
+    
+    parser.add_argument("--version", action="version",version="%(prog)s v{version}".format(version=__version__))
+    parser.add_argument("-f", "--file", type=str, help="SAXS data file for input (either .dat or .out)")
+    parser.add_argument("-d", "--dmax", default=None, type=float, help="Estimated maximum dimension")
+    parser.add_argument("-v", "--voxel", default=None, type=float, help="Set desired voxel size, setting resolution of map")
+    parser.add_argument("-os","--oversampling", default=3., type=float, help="Sampling ratio")
+    parser.add_argument("-n", "--nsamples", default=None, type=int, help="Number of samples, i.e. grid points, along a single dimension. (Sets voxel size, overridden by --voxel. Best optimization with n=power of 2)")
+    parser.add_argument("--ne", default=10000, type=float, help="Number of electrons in object")
+    parser.add_argument("-s", "--steps", default=None, help="Maximum number of steps (iterations)")
+    parser.add_argument("-o", "--output", default=None, help="Output map filename")
+    parser.add_argument("-m", "--mode", default="SLOW", type=str, help="Mode. F(AST) sets default options to run quickly for simple particle shapes. S(LOW) useful for more complex molecules. (default SLOW)")
+    parser.add_argument("--seed", default=None, help="Random seed to initialize the map")
+    parser.add_argument("-ld_on","--limit_dmax_on", dest="limit_dmax", action="store_true", help="Limit electron density to sphere of radius 0.6*Dmax from center of object.")
+    parser.add_argument("-ld_off","--limit_dmax_off", dest="limit_dmax", action="store_false", help="Do not limit electron density to sphere of radius 0.6*Dmax from center of object. (default)")
+    parser.add_argument("-ld_steps","--limit_dmax_steps", default=None, type=int, nargs='+', help="List of steps for limiting density to sphere of Dmax (default=500)")
+    parser.add_argument("-rc_on", "--recenter_on", dest="recenter", action="store_true", help="Recenter electron density when updating support. (default)")
+    parser.add_argument("-rc_off", "--recenter_off", dest="recenter", action="store_false", help="Do not recenter electron density when updating support.")
+    parser.add_argument("-rc_steps", "--recenter_steps", default=None, type=int, nargs='+', help="List of steps to recenter electron density.")
+    parser.add_argument("-rc_mode", "--recenter_mode", default="com", type=str, help="Recenter based on either center of mass (com, default) or maximum density value (max)")
+    parser.add_argument("-p_on","--positivity_on", dest="positivity", action="store_true", help="Enforce positivity restraint inside support. (default)")
+    parser.add_argument("-p_off","--positivity_off", dest="positivity", action="store_false", help="Do not enforce positivity restraint inside support.")
+    parser.add_argument("-min","--minimum_density", default=None, type=float, help="Minimum density value in e-/angstrom^3 (must also set --ne to be meaningful)")
+    parser.add_argument("-max","--maximum_density", default=None, type=float, help="Maximum density value in e-/angstrom^3 (must also set --ne to be meaningful)")
+    parser.add_argument("-e_on","--extrapolate_on", dest="extrapolate", action="store_true", help="Extrapolate data by Porod law to high resolution limit of voxels. (default)")
+    parser.add_argument("-e_off","--extrapolate_off", dest="extrapolate", action="store_false", help="Do not extrapolate data by Porod law to high resolution limit of voxels.")
+    parser.add_argument("-sw_on","--shrinkwrap_on", dest="shrinkwrap", action="store_true", help="Turn shrinkwrap on (default)")
+    parser.add_argument("-sw_off","--shrinkwrap_off", dest="shrinkwrap", action="store_false", help="Turn shrinkwrap off")
+    parser.add_argument("-sw_start","--shrinkwrap_sigma_start", default=3, type=float, help="Starting sigma for Gaussian blurring, in voxels")
+    parser.add_argument("-sw_end","--shrinkwrap_sigma_end", default=1.5, type=float, help="Ending sigma for Gaussian blurring, in voxels")
+    parser.add_argument("-sw_decay","--shrinkwrap_sigma_decay", default=0.99, type=float, help="Rate of decay of sigma, fraction")
+    parser.add_argument("-sw_threshold","--shrinkwrap_threshold_fraction", default=0.20, type=float, help="Minimum threshold defining support, in fraction of maximum density")
+    parser.add_argument("-sw_iter","--shrinkwrap_iter", default=20, type=int, help="Number of iterations between updating support with shrinkwrap")
+    parser.add_argument("-sw_minstep","--shrinkwrap_minstep", default=None, type=int, help="First step to begin shrinkwrap")
+    parser.add_argument("-ec_on","--enforce_connectivity_on", dest="enforce_connectivity", action="store_true", help="Enforce connectivity of support, i.e. remove extra blobs (default)")
+    parser.add_argument("-ec_off","--enforce_connectivity_off", dest="enforce_connectivity", action="store_false", help="Do not enforce connectivity of support")
+    parser.add_argument("-ec_steps","--enforce_connectivity_steps", default=None, type=int, nargs='+', help="List of steps to enforce connectivity")
+    parser.add_argument("--chi_end_fraction", default=0.001, type=float, help="Convergence criterion. Minimum threshold of chi2 std dev, as a fraction of the median chi2 of last 100 steps.")
+    parser.add_argument("--write_xplor", default=False, action="store_true", help="Write out XPLOR map format (default only write MRC format).")
+    parser.add_argument("--write_freq", default=100, type=int, help="How often to write out current density map (in steps, default 100).")
+    parser.add_argument("--cutout_on", dest="cutout", action="store_true", help="When writing final map, cut out the particle to make smaller files.")
+    parser.add_argument("--cutout_off", dest="cutout", action="store_false", help="When writing final map, do not cut out the particle to make smaller files (default).")
+    parser.add_argument("--plot_on", dest="plot", action="store_true", help="Create simple plots of results (requires Matplotlib, default if module exists).")
+    parser.add_argument("--plot_off", dest="plot", action="store_false", help="Do not create simple plots of results. (Default if Matplotlib does not exist)")
+    parser.set_defaults(limit_dmax=False)
+    parser.set_defaults(shrinkwrap=True)
+    parser.set_defaults(recenter=True)
+    parser.set_defaults(positivity=True)
+    parser.set_defaults(extrapolate=True)
+    parser.set_defaults(enforce_connectivity=True)
+    parser.set_defaults(cutout=False)
+    if matplotlib_found:
+        parser.set_defaults(plot=True)
+    else:
+        parser.set_defaults(plot=False)
+    args = parser.parse_args()
+
+    if args.output is None:
+        basename, ext = os.path.splitext(args.file)
+        args.output = basename
+    else:
+        args.output = args.output
+
+    #A bug appears to be present when disabling the porod extrapolation.
+    #for now that option will be disabled until I come up with a fix
+    if args.extrapolate is False:
+        print ("There is currently a bug when disabling the Porod "
+               "extrapolation (the -e_off option). \n For now, extrapolation "
+               "has been re-enabled until a bug fix is released. ")
+        args.extrapolate = True
+
+    #for FAST or SLOW modes, set some default values for a few options
+    if args.mode[0].upper() == "F":
+        args.mode = "FAST"
+        nsamples = 32
+        shrinkwrap_minstep = 1000
+        enforce_connectivity_steps = [2000]
+        recenter_steps = range(501,2502,500)
+        steps = 5000
+    elif args.mode[0].upper() == "S":
+        args.mode = "SLOW"
+        nsamples = 64
+        shrinkwrap_minstep = 5000
+        enforce_connectivity_steps = [6000]
+        recenter_steps = range(501,8002,500)
+        steps = 10000
+    else:
+        args.mode = "None"
+
+    #allow user to explicitly modify those values by resetting them here to the user defined values
+    if args.nsamples is not None:
+        nsamples = args.nsamples
+
+    if args.shrinkwrap_minstep is not None:
+        shrinkwrap_minstep = args.shrinkwrap_minstep
+
+    if args.enforce_connectivity_steps is not None:
+        enforce_connectivity_steps = args.enforce_connectivity_steps
+    if not isinstance(enforce_connectivity_steps, list):
+        enforce_connectivity_steps = [ enforce_connectivity_steps ]
+
+    if args.recenter_steps is not None:
+        recenter_steps = args.recenter_steps
+    if not isinstance(recenter_steps, list):
+        recenter_steps = [ recenter_steps ]
+
+    if args.limit_dmax_steps is not None:
+        limit_dmax_steps = args.limit_dmax_steps
+    else:
+        limit_dmax_steps = [500]
+    if not isinstance(limit_dmax_steps, list):
+        limit_dmax_steps = [ limit_dmax_steps ]
+
+    if args.steps is not None:
+        steps = args.steps
+
+    if args.dmax is not None:
+        dmax = args.dmax
+    elif dmax<=0:
+        dmax = 100
+
+    if args.voxel is None and nsamples is None:
+        voxel = 5.
+    elif args.voxel is None and nsamples is not None:
+        voxel = dmax * args.oversampling / nsamples
+    else:
+        voxel = args.voxel
+
+    #now recollect all the edited options back into args
+    args.nsamples = nsamples
+    args.shrinkwrap_minstep = shrinkwrap_minstep
+    args.enforce_connectivity_steps = enforce_connectivity_steps
+    args.recenter_steps = recenter_steps
+    args.limit_dmax_steps = limit_dmax_steps
+    args.steps = steps
+    args.dmax = dmax
+    args.voxel = voxel
+
+    return args
+
