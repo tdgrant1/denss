e3cc223c0ebb2a29befdf3bef34ac2ca2be28479
Thomas D. Grant <tgrant@hwi.buffalo.edu>
2018-11-21 12:50:04 -0500

    Updated denss.get_info.py to print side length and voxel size in all three axes.

--- bin/denss.get_info.py
+++ bin/denss.get_info.py
@@ -41,11 +41,12 @@ args = parser.parse_args()
 if __name__ == "__main__":
 
 
-    rho, side = saxs.read_mrc(args.file)
+    rho, (a,b,c) = saxs.read_mrc(args.file, returnABC=True)
+    vx, vy, vz = np.array((a,b,c))/np.array(rho.shape)
     
     print " Grid size:   %i x %i x %i" % (rho.shape[0],rho.shape[1],rho.shape[2])
-    print " Side length: %f" % side
-    print " Voxel size:  %f" % (side/rho.shape[0])
+    print " Side length: %f x %f x %f" % (a,b,c)
+    print " Voxel size:  %f x %f x %f" % (vx, vy, vz)
 
 
 
